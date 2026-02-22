19271ad663998bdf2d683802d342aa0b0920f98c
salim <salim.belkacemm@protonmail.com>
2026-02-22 20:00:05 +0100

    fuckass archi stuff

---
+++ archi/tp/myapp/src/main/java/com/tp1/something.java
@@ -0,0 +1,16 @@
+package com.tp1;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class something extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello from Servlets!</h1>");
+    }
+}
